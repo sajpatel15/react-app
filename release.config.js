8d7ffd71f09560af74504d967c5768aac06cdc24
sajpatel15 <sajpatel64@gmail.com>
2022-09-09 13:45:05 -0600

    fix: fix description

--- release.config.js
+++ release.config.js
@@ -2,9 +2,9 @@ module.exports = {
     branches: "master",
     repositoryURL: "https://github.com/sajpatel15/react-app"
     plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github'
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        '@semantic-release/github'
     ]
 }
